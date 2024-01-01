5aa32a45e18ed212657a131592c60175a259b991
Ryo <kanekuni0104@gmail.com>
2024-01-01 18:28:52 +0900

    bootstrapを一部適用した

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static((__dirname+'public')));
 
 
 app.use('/',homeRoutes);
