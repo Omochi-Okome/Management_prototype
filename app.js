5bd784da2eda3a85c4d6714f0e75d4c9414bc867
Ryo <kanekuni0104@gmail.com>
2024-01-01 12:58:23 +0900

    404Foundを追加

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const homeRoutes = require('./routes/home');
 const adminRoutes = require('./routes/admin');
+const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
 
 require("dotenv").config();
@@ -13,8 +14,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/',homeRoutes);
 app.use('/',adminRoutes);
+app.use(errorController.get404);
 
 mongoConnect(() => {
     app.listen(3000);
